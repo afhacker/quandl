82093273da85e385d0a5a7b9aba6a83cb54339f6
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2018-04-13 12:51:45 +0430

    BaseURL fields changed to static from const and some methods in RequestUtility class has been changed to one line return.

---
+++ EasyQuandl/DataSet.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace EasyQuandl
+{
+    public class DataSet
+    {
+        [JsonProperty("id")]
+        public long ID { get; set; }
+
+        [JsonProperty("dataset_code")]
+        public string Code { get; set; }
+
+        [JsonProperty("database_code")]
+        public string DatabaseCode { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("refreshed_at")]
+        public DateTimeOffset RefreshedAt { get; set; }
+
+        [JsonProperty("newest_available_date")]
+        public DateTime NewestAvailableDate { get; set; }
+
+        [JsonProperty("oldest_available_date")]
+        public DateTime OldestAvailableDate { get; set; }
+
+        [JsonProperty("column_names")]
+        public List<string> ColumnNames { get; set; }
+
+        [JsonProperty("frequency")]
+        public string Frequency { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("premium")]
+        public bool Premium { get; set; }
+
+        [JsonProperty("limit")]
+        public long? Limit { get; set; }
+
+        [JsonProperty("transform")]
+        public string Transform { get; set; }
+
+        [JsonProperty("column_index")]
+        public int? ColumnIndex { get; set; }
+
+        [JsonProperty("start_date")]
+        public DateTime StartDate { get; set; }
+
+        [JsonProperty("end_date")]
+        public DateTime EndDate { get; set; }
+
+        [JsonProperty("data")]
+        public IList Data { get; set; }
+
+        [JsonProperty("collapse")]
+        public string Collapse { get; set; }
+
+        [JsonProperty("order")]
+        public string Order { get; set; }
+
+        [JsonProperty("database_id")]
+        public long DatabaseID { get; set; }
+    }
+}
---
+++ EasyQuandl/DataTable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace EasyQuandl
+{
+    public class DataTable
+    {
+        [JsonProperty("data")]
+        public IList Data { get; set; }
+
+        [JsonProperty("columns")]
+        public List<DataTableColumn> Columns { get; set; }
+    }
+}
