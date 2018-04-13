d84f52ae47db8d853e88691beefb5610bffb51e5
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2018-04-13 12:56:08 +0430

    EasyQuandl added.

---
+++ EasyQuandl/DataTableStatus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace EasyQuandl
+{
+    public class DataTableStatus
+    {
+        [JsonProperty("refreshed_at")]
+        public DateTimeOffset RefreshedAt { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("expected_at")]
+        public DateTime? ExpectedAt { get; set; }
+
+        [JsonProperty("update_frequency")]
+        public string UpdateFrequency { get; set; }
+    }
+}
